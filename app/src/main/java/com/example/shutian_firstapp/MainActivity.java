9d9119e3711c1e3c9573e9a111ec590b27e09cab
shutianlin <54730703+shutianlin@users.noreply.github.com>
2021-02-24 10:24:30 +0800

    Lab2.1b

--- app/src/main/java/com/example/shutian_firstapp/MainActivity.java
+++ app/src/main/java/com/example/shutian_firstapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.shutian_firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
